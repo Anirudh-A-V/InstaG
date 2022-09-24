10afb9d141376eec986b983d0a89a76923a94e94
Anirudh A V <anirudh.av02@gmail.com>
2022-09-24 14:27:13 +0530

    useStorage hook created > useStorage hook is used to handle file uploads and return useful values like the file upload progress, url and error message(if any).

--- src/comps/Form.js
+++ src/comps/Form.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Form = () => {
+
+    const [file, setFile] = useState(null);
+    const [error, setError] = useState(null);
+
+    const types = ['image/png', 'image/jpeg', 'image/jpg', 'image/svg', 'image/tif'];
+    
+    const changeHandler = (e) => {
+        let selected = e.target.files[0];
+        console.log(selected);
+
+        if(selected && types.includes(selected.type)){
+            setFile(selected);
+            setError('');
+        } else {
+            setFile(null);
+            setError('Please select an image file (png/jpeg/jpg/svg/tif)');
+        }
+    }
+
     return (
         <form>
-            <input type="file" />
+            <input type="file" onChange={changeHandler} />
+            <div className="output">
+                { error && <div className="error">{ error }</div> }
+                { file && <div>{ file.name }</div> }
+            </div>
         </form>
 
     );
