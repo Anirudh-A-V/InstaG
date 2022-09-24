cd7d2178769cd949661133f765cb3013313791c4
Anirudh A V <anirudh.av02@gmail.com>
2022-09-24 12:11:12 +0530

    Upload form added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Title from './comps/Title';
+import Form from './comps/Form';
 
 function App() {
   return (
     <div className="App">
       <Title/>
+      <Form />
     </div>
   );
 }
---
+++ src/comps/Form.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Form = () => {
+    return (
+        <form>
+            <input type="file" />
+        </form>
+
+    );
+}
+
+export default Form;
