1f0ad7594d02a0c87508578c2a5cc0c96bcae1c0
Anirudh A V <anirudh.av02@gmail.com>
2022-09-24 12:06:15 +0530

    initial commit

---
+++ src/comps/Title.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>InstaG</h1>
+      <h2>Your Pics</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </div>
+  )
+}
+
+export default Title;
