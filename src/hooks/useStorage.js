59e4c13e8b4c1a9fa21f1d3a7970c0c8dc47c3a9
Anirudh A V <anirudh.av02@gmail.com>
2022-09-28 17:46:52 +0530

    Firestore fixed > addDoc() was defined inside the getDownloadURL() function. As such the url was able to be added to the firestore.

--- src/hooks/useStorage.js
+++ src/hooks/useStorage.js
@@ -1,33 +1,20 @@
 import { useState, useEffect } from 'react';
 import { projectStorage, projectFirestore, timestamp } from '../firebase/config';
-import { ref, uploadBytesResumable, getDownloadURL, getMetadata } from "firebase/storage";
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { collection, addDoc } from "firebase/firestore";
 
 const useStorage = (file) => {
     const [progress, setProgress] = useState(0);
     const [error, setError] = useState(null);
     const [url, setUrl] = useState(null);
-    const [time, setTime] = useState(null);
 
-    
+
     useEffect(() => {
         // references
         const storageRef = ref(projectStorage, file.name);
         const collectionRef = collection(projectFirestore, 'images');
 
         const uploadImage = uploadBytesResumable(storageRef, file);
-
-        getMetadata(storageRef)
-            .then((metadata) => {
-                // console.log(metadata);
-                // setTime(metadata['timeCreated']);
-                const timeVar = metadata['timeCreated'];
-                console.log(timeVar);
-                setTime(timeVar);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
 
         uploadImage.on('state_changed', (snap) => {
             let percentage = (snap.bytesTransferred / snap.totalBytes) * 100;
@@ -36,24 +23,26 @@ const useStorage = (file) => {
         }, (err) => {
             setError(err);
         }, async () => {
-            const url = await getDownloadURL(uploadImage.snapshot.ref).then((URL) => {
+            await getDownloadURL(uploadImage.snapshot.ref).then((URL) => {
                 console.log(URL);
+                const data = {
+                    url: URL,
+                    createdAt: timestamp
+                }
+                setUrl(URL);
+                console.log(data);
+                addDoc(collectionRef, data)
+                    .then(docRef => {
+                        console.log("Document has been added successfully");
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    })
+
             });
-            const createdAt = time;
-            const data = {
-                url: url,
-                createdAt: createdAt
-            }
-            await addDoc(collectionRef, data)
-                .then(docRef => {
-                    console.log("Document has been added successfully");
-                })
-                .catch(error => {
-                    console.log(error);
-                })
-            setUrl(url);
+
         })
-    }, [file], time, setUrl, setTime);
+    }, [file]);
 
     return { progress, url, error }
 }
