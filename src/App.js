947071abb931087ae941995dbddcf33700739e8a
Anirudh A V <anirudh.av02@gmail.com>
2022-09-28 21:31:34 +0530

    Modal added > By clicking an image in the gallery, a modal will appear with the image in full size. > Then by clicking the background, the modal will disappear.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React from 'react';
 import Title from './comps/Title';
 import Form from './comps/Form';
 import ImageGrid from './comps/ImageGrid';
+import Modal from './comps/Modal';
 
 function App() {
+
+  const [selectedImg, setSelectedImg] = React.useState(null);
+
   return (
     <div className="App">
       <Title/>
       <Form />
-      <ImageGrid />
+      <ImageGrid setSelectedImg={setSelectedImg}/>
+      { selectedImg && <Modal setSelectedImg={setSelectedImg} selectedImg={selectedImg}/>}
     </div>
   );
 }
---
+++ src/comps/Modal.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Modal = ({ selectedImg, setSelectedImg }) => {
+
+    const handleClick = (e) => {
+        if (e.target.classList.contains('backdrop')) {
+            setSelectedImg(null);
+        }
+    }
+
+    return (
+        <div className="backdrop"
+            onClick={handleClick}>
+            <img src={selectedImg} alt="modal image" />
+        </div>
+    );
+}
+
+export default Modal;
