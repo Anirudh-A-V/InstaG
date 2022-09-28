72a63c6f3165da1cea20c860bb79369d442942d7
Anirudh A V <anirudh.av02@gmail.com>
2022-09-28 19:22:54 +0530

    Image grid built with CSS Grid

--- src/comps/ImageGrid.js
+++ src/comps/ImageGrid.js
@@ -7,6 +7,11 @@ const ImageGrid = () => {
 
     return (
         <div className="img-grid">
+            {docs && docs.map(doc => (
+                <div className="img-wrap" key={doc.id}>
+                    <img src={doc.url} alt="uploaded pic" />
+                </div>
+            ))}
         </div>
     );
 }
