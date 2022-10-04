2752b7a8637e90369a73684a4c1fde44bd9dd226
Anirudh A V <anirudh.av02@gmail.com>
2022-10-04 15:50:06 +0530

    Few edits > Title changes > Color changes > Photos changed

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
     <div className="title">
       <h1>InstaG</h1>
       <h2>Your Pics</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>Upload your pics here.</p>
     </div>
   )
 }
