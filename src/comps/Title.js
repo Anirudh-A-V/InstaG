dc43bc3b96caeed6c73531733c1643843009d14b
Anirudh A V <anirudh.av02@gmail.com>
2022-10-15 18:27:33 +0530

    Styles updated

--- src/comps/Title.js
+++ src/comps/Title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return (
     <div className="title">
-      <h1>InstaG</h1>
+      <h1>Gallery</h1>
       <h2>Your Pics</h2>
       <p>Upload your pics here.</p>
     </div>
