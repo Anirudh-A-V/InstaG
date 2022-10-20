e92b0aafebf87ecbadad1238ca9a35268c00f66a
Anirudh A V <anirudh.av02@gmail.com>
2022-10-20 11:39:13 +0530

    getAuth setup

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, serverTimestamp } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,8 +20,11 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
 const projectFirestore= getFirestore(app);
 const projectStorage = getStorage(app);
 const timestamp = serverTimestamp();
 
-export { projectStorage, projectFirestore, timestamp };
+export { projectStorage, projectFirestore, timestamp, auth };
