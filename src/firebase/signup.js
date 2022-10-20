ed102cd3c9b1fc122281218e604149c7c0f2d766
Anirudh A V <anirudh.av02@gmail.com>
2022-10-20 11:40:08 +0530

    signup function partially configured

---
+++ src/firebase/signup.js
@@ -0,0 +1,15 @@
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
+import { auth } from "./config";
+
+createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // ..
+    });
